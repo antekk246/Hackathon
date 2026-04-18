844fd44348000845dffbd80b97cd3d76c509cee7
Tomasz Skrzyp <Tomasz Skrzyp@DESKTOP-AMBO86B>
2026-04-18 14:05:49 +0200

    fix: oauth now works

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,26 +4,35 @@ import (
 	"backend/internal/api"
 	"backend/internal/domain"
 	"backend/internal/handler"
-	"backend/internal/models" // upewnij się, że ścieżka jest poprawna
+	"backend/internal/models"
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
+
+	"github.com/glebarez/sqlite"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	log.Println("Initializing SQLite database...")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, reading from system environment")
+	}
+	requiredEnvs := []string{"GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", "GOOGLE_REDIRECT_URL"}
+	for _, env := range requiredEnvs {
+		if os.Getenv(env) == "" {
+			log.Fatalf("Fatal error: Environment variable %s is not set!", env)
+		}
+	}
 
-	// 1. Połączenie z SQLite (plik hackathon.db powstanie automatycznie)
+	log.Println("Initializing SQLite database...")
 	db, err := gorm.Open(sqlite.Open("hackathon.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	// 2. Automatyczna migracja - GORM sam stworzy tabelę users na podstawie modelu
 	err = db.AutoMigrate(&models.User{})
 	if err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
@@ -40,7 +49,6 @@ func main() {
 	userRepo := domain.NewUserRepository(db)
 	authHandler := handler.NewAuthHandler(userRepo, oauthConf)
 
-	// 5. Start serwera
 	r := api.SetupRouter(authHandler)
 	log.Println("Server starting on :8080...")
 	if err := r.Run(":8080"); err != nil {
