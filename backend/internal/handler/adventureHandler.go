d3b0fc40fe3d1b6cabb58b61a404aa0f0d244628
akubek <artur.kubek.96@gmail.com>
2026-04-19 02:31:47 +0200

    add card list to error msg

--- backend/internal/handler/adventureHandler.go
+++ backend/internal/handler/adventureHandler.go
@@ -112,7 +112,7 @@ func (h *AdventureHandler) StartAdventure(c *gin.Context) {
 
 	if err := h.Repo.Create(&newAdventure); err != nil {
 		// INCLUDE THE ERROR HERE:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to start adventure: " + err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to start adventure: " + err.Error() + "with cards: " + fmt.Sprint(cards)})
 		return
 	}
 
